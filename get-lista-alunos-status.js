bf14bf85ff9aa4d98ea12793fdd944570eeafe00
HasegawaTaizou <caiocoghi@gmail.com>
2023-03-27 11:20:52 -0300

    alo

--- get-lista-alunos-status.js
+++ get-lista-alunos-status.js
@@ -2,9 +2,14 @@
 
 const alunos = require("./alunos.js");
 
+
+
 const getListaAlunosStatus = function (statusEspecifico) {
-    
+  let alunosFiltrados = alunos.alunos.filter(aluno => {
+    return aluno.status == statusEspecifico
+  })
+  return alunosFiltrados
 };
 
+console.log(getListaAlunosStatus("Finalizado"));
 
-getListaAlunosStatus("RDS")
